390afd498d940d08c44304f922e337e31e1a8bf9
Clavin Fernandes <cfernandes@muhimbi.com>
2023-03-18 01:44:09 +0530

    Added React PCF Component

---
+++ ShakingText/generated/ManifestTypes.d.ts
@@ -0,0 +1,14 @@
+/*
+*This is auto generated from the ControlManifest.Input.xml file
+*/
+
+// Define IInputs and IOutputs Type. They should match with ControlManifest.
+export interface IInputs {
+    dummytext: ComponentFramework.PropertyTypes.StringProperty;
+    inputtext: ComponentFramework.PropertyTypes.StringProperty;
+    fontSize: ComponentFramework.PropertyTypes.StringProperty;
+    color: ComponentFramework.PropertyTypes.StringProperty;
+}
+export interface IOutputs {
+    dummytext?: string;
+}
